c4777a6d445360b27e4787071ffe29f42a8054aa
Mohammed Emam <mopilani1@gmail.com>
2025-08-24 20:21:34 +0200

    Loop service

--- bin/lib/pop3_service_loop.dart
+++ bin/lib/pop3_service_loop.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:enough_mail/enough_mail.dart';
 import 'print_message_function.dart';
 import 'db_implementions.dart';
@@ -25,16 +28,22 @@ Future<void> popServiceLoop({
     );
 
     final messageList = await P3Client.list();
-    for (var msgRef in messageList) {
-      print('Message Id: ${msgRef.id}');
-      var message = await P3Client.retrieve(msgRef.id);
-      print(message);
-      print(
-        '-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-',
-      );
-      printMessage(message);
-      // var _message = P3Client.decodeMessage(message);
-      // await P3Db().addMail(msgRef.id, message);
+
+    for (int i = 0; i < 10; i++) {
+      for (var msgRef in messageList) {
+        print('Message Id: ${msgRef.id}');
+        var message = await P3Client.retrieve(msgRef.id);
+        print(message);
+        print(
+          '-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-',
+        );
+        printMessage(message);
+        // var _message = P3Client.decodeMessage(message);
+        // await P3Db().addMail(msgRef.id, message);
+      }
+      await Future.delayed(Duration(seconds: 60), () {
+        print('In Delay +');
+      });
     }
   } catch (e, s) {
     print(e);
